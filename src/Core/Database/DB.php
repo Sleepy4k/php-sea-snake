581e42055393d495c0639f105a1e05ac1b5fcc3c
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-09-20 20:01:10 +0700

    chore: main database

--- src/Core/Database/DB.php
+++ src/Core/Database/DB.php
@@ -2,216 +2,87 @@
 
 namespace Snake\Core\Database;
 
-use PDO;
-use PDOException;
+use Snake\Core\Facade\App;
 
-class DB {
+final class DB {
   /**
-   * Object instance
-   *
-   * @var object $pdo
-   */
-  private $pdo;
-
-  /**
-   * Create a new instance
-   *
-   * @return void
-   *
-   * @throws PDOException
-   */
-  public function __construct() {
-    if ($this->pdo === null) {
-      try {
-        $this->pdo = new PDO(
-          'mysql:host=' . config('database', 'hostname') . ';dbname=' . config('database', 'name'),
-          config('database', 'username'),
-          config('database', 'password')
-        );
-      } catch (PDOException $e) {
-        die($e->getMessage());
-      }
-    }
-  }
-
-  /**
-   * Get a PDO instance
-   *
-   * @return object
-   */
-  public function pdo() {
-    return $this->pdo;
-  }
-
-  /**
-   * Handle database query
-   *
+   * Query the database
+   * 
    * @param string $sql
    * @param array $params
-   *
+   * 
    * @return object
    */
-  public function query(string $sql = '', array $params = []) {
-    $stmt = $this->pdo->prepare($sql);
-    $stmt->execute($params);
-
-    return $stmt;
+  public static function query(string $sql = '', array $params = []) {
+    return static::builder()->query($sql, $params);
   }
 
   /**
-   * Get all records
-   *
+   * Get all data from a table
+   * 
    * @param string $table
-   *
+   * 
    * @return object
    */
-  public function all(string $table = '') {
-    $stmt = $this->pdo->prepare('SELECT * FROM ' . $table);
-    $stmt->execute();
-
-    return $stmt;
+  public static function all(string $table = '') {
+    return static::builder()->all($table);
   }
 
   /**
-   * Get a record
-   *
+   * Get data from a table
+   * 
    * @param string $table
    * @param array $where
-   *
+   * 
    * @return object
    */
-  public function get(string $table = '', array $where = []) {
-    $sql = 'SELECT * FROM ' . $table . ' WHERE ';
-    $i = 0;
-
-    foreach ($where as $key => $value) {
-      $i++;
-      $sql .= $key . ' = :' . $key;
-
-      if ($i < count($where)) {
-        $sql .= ' AND ';
-      }
-    }
-
-    $stmt = $this->pdo->prepare($sql);
-    $stmt->execute($where);
-
-    return $stmt;
+  public static function get(string $table = '', array $where = []) {
+    return static::builder()->get($table, $where);
   }
 
   /**
    * Insert a record
-   *
+   * 
    * @param string $table
    * @param array $data
-   *
+   * 
    * @return object
    */
-  public function insert(string $table = '', array $data = []) {
-    $sql = 'INSERT INTO ' . $table . ' (';
-    $i = 0;
-
-    foreach ($data as $key => $value) {
-      $i++;
-      $sql .= $key;
-
-      if ($i < count($data)) {
-        $sql .= ', ';
-      }
-    }
-
-    $sql .= ') VALUES (';
-    $i = 0;
-
-    foreach ($data as $key => $value) {
-      $i++;
-      $sql .= ':' . $key;
-
-      if ($i < count($data)) {
-        $sql .= ', ';
-      }
-    }
-
-    $sql .= ')';
-    $stmt = $this->pdo->prepare($sql);
-    $stmt->execute($data);
-
-    return $stmt;
+  public static function insert(string $table = '', array $data = []) {
+    return static::builder()->insert($table, $data);
   }
 
   /**
    * Update a record
-   *
+   * 
    * @param string $table
    * @param array $data
    * @param array $where
-   *
+   * 
    * @return object
    */
-  public function update(string $table = '', array $data = [], array $where = []) {
-    $sql = 'UPDATE ' . $table . ' SET ';
-    $i = 0;
-
-    foreach ($data as $key => $value) {
-      $i++;
-      $sql .= $key . ' = :' . $key;
-
-      if ($i < count($data)) {
-        $sql .= ', ';
-      }
-    }
-
-    $sql .= ' WHERE ';
-    $i = 0;
-
-    foreach ($where as $key => $value) {
-      $i++;
-      $sql .= $key . ' = :' . $key;
-
-      if ($i < count($where)) {
-        $sql .= ' AND ';
-      }
-    }
-
-    $stmt = $this->pdo->prepare($sql);
-    $stmt->execute(array_merge($data, $where));
-
-    return $stmt;
+  public static function update(string $table = '', array $data = [], array $where = []) {
+    return static::builder()->update($table, $data, $where);
   }
 
   /**
    * Delete a record
-   *
+   * 
    * @param string $table
    * @param array $where
-   *
+   * 
    * @return object
    */
-  public function delete(string $table = '', array $where = []) {
-    $sql = 'DELETE FROM ' . $table . ' WHERE ';
-    $i = 0;
-
-    foreach ($where as $key => $value) {
-      $i++;
-      $sql .= $key . ' = :' . $key;
-
-      if ($i < count($where)) {
-        $sql .= ' AND ';
-      }
-    }
-
-    $stmt = $this->pdo->prepare($sql);
-    $stmt->execute($where);
-    
-    return $stmt;
+  public static function delete(string $table = '', array $where = []) {
+    return static::builder()->delete($table, $where);
   }
 
   /**
-   * Get the last inserted ID
+   * Get router instance
    *
-   * @return string
+   * @return Builder
    */
-  public function lastInsertId() {
-    return $this->pdo->lastInsertId();
+  public static function builder(): Builder {
+    return App::get()->singleton(Builder::class);
   }
 }
