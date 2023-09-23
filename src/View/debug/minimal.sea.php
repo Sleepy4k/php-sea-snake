3e74067d63ae09185ba2ec5111a90f079eb2ff35
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-09-23 13:09:29 +0700

    chore: minimal debug view

--- src/View/debug/minimal.sea.php
+++ src/View/debug/minimal.sea.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title><?= config('bin', 'name') ?> | Dump</title>
+
+    <link rel="apple-touch-icon" href="<?= asset('logo.png') ?>">
+    <link rel="shortcut icon" href="<?= asset('logo.png') ?>" type="image/x-icon">
+
     <style>
       pre {
         overflow: auto;
