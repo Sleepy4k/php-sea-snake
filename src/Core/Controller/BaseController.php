a41c7dd5487280dc226287d7b698743dce7965c1
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-09-23 13:10:54 +0700

    chore: base controller core

--- src/Core/Controller/BaseController.php
+++ src/Core/Controller/BaseController.php
@@ -6,9 +6,11 @@ use Snake\Core\View\Sea;
 use Snake\Core\Facade\App;
 use BadMethodCallException;
 use Snake\Core\Http\Request;
+use Snake\Core\Http\Response;
 use Snake\Core\Http\Validator;
+use Snake\Interface\Controller\IController;
 
-class BaseController {
+class BaseController implements IController {
   /**
    * Validate the request
    *
@@ -36,40 +38,39 @@ class BaseController {
   /**
    * Response the data
    *
-   * @param array $data
+   * @param mixed $data
+   * @param array $header
    * @param int $status
    *
    * @return void
    */
-  public static function response(array $data, int $status = 200): void {
-    http_response_code($status);
-    header('Content-Type: application/json');
-    echo json_encode($data);
+  public static function response(mixed $data, array $header = [], int $status = 200): void {
+    Response::json($data, $header, $status);
   }
 
   /**
    * Execute an action on the controller.
    *
-   * @param  string  $method
-   * @param  array  $parameters
+   * @param string $method
+   * @param array $parameters
    *
    * @return \Symfony\Component\HttpFoundation\Response
    */
-  public function callAction($method, $parameters) {
+  public function callAction(string $method, array $parameters) {
     return $this->{$method}(...array_values($parameters));
   }
 
   /**
    * Handle calls to missing methods on the controller.
    *
-   * @param  string  $method
-   * @param  array  $parameters
+   * @param string $method
+   * @param array $parameters
    *
    * @return mixed
    *
    * @throws \BadMethodCallException
    */
-  public function __call($method, $parameters){
+  public function __call(string $method, array $parameters): mixed {
     if (!method_exists($this, $method)) {
       if (config('bin', 'debug') && config('bin', 'env') === 'development') {
         throw new BadMethodCallException(sprintf(
